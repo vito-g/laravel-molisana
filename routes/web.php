5f43b8487ba802397e6063c0497997045b3ca315
Vito Gentile <gentilevito1076@gmail.com>
2021-03-11 16:07:21 +0100

    Homepage.blade + Product.blade con json

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
