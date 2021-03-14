0299650c76b243a5f841189a60b9f0c07a6f9530
Vito Gentile <gentilevito1076@gmail.com>
2021-03-14 17:27:55 +0100

    Istruzione di esempio messa a commento

--- resources/views/components/product-content.blade.php
+++ resources/views/components/product-content.blade.php
@@ -132,7 +132,15 @@ $data = json_decode($data, true); //Il true si mette perchè l'array è associat
 @extends('layout.molisana')
 @section('content-main')
 
-  questa è la pagina prodotto {{$idProduct + 1}}
-  {{ $data[$idProduct]['descrizione'] }}
-  
+  <div class="zoomIn-product">
+
+    {{-- questa è la pagina prodotto {{$idProduct + 1}} --}}
+    {{-- {{ $data[$idProduct]['descrizione'] }} --}}
+    <img src="{{ $data[$idProduct]['src-h'] }}" alt="">
+    <img src="{{ $data[$idProduct]['src-p'] }}" alt="">
+
+    {{-- {{ $data[$idProduct]['src-p'] }} --}}
+
+  </div>
+
 @endsection
